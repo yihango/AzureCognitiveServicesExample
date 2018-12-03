97d66107c26bf7d9f7ffb43d186e024fe2f0a698
stan e <yi.hang@live.com>
2018-12-03 14:59:26 +0800

    图片分析和OCR修改

--- ComputerVisionImgAnalyze/Program.cs
+++ ComputerVisionImgAnalyze/Program.cs
@@ -9,18 +9,20 @@ using System.Threading.Tasks;
 
 namespace ComputerVisionImgAnalyze
 {
+    /// <summary>
+    /// 图片场景分析
+    /// </summary>
     class Program
     {
         // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
-        private const string apiKey = "<SubscriptionKey>";
+        private const string apiKey = "";
 
         // localImagePath = @"C:\Documents\LocalImage.jpg"
-        private const string localImagePath = @"<LocalImage>";
+        private const string localImagePath = @"imgs/1.jpg";
 
-        private const string remoteImageUrl =
-            "http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg";
+        private const string remoteImageUrl = "http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg";
 
-        // Specify the features to return
+        // 指定要返回的特性
         private static readonly List<VisualFeatureTypes> features =
             new List<VisualFeatureTypes>()
         {
@@ -35,22 +37,19 @@ namespace ComputerVisionImgAnalyze
                 new ApiKeyServiceClientCredentials(apiKey),
                 new System.Net.Http.DelegatingHandler[] { });
 
-            /*
-             You must use the same region as you used to get your subscription keys.
-             For example, if you got your subscription keys from westus,replace "westcentralus" with "westus".
-             Free trial subscription keys are generated in the "westus" region. 
-             If you use a free trial subscription key, you shouldn't need to change the region.
-             */
-
             // 指定Azure区域
             computerVision.Endpoint = "https://westcentralus.api.cognitive.microsoft.com";
 
             Console.WriteLine("分析图像中 ...");
-            var t1 = AnalyzeRemoteAsync(computerVision, remoteImageUrl);
-            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
 
-            Task.WhenAll(t1, t2).Wait(5000);
-            Console.WriteLine("按ENTER键退出");
+            // 远程图片
+            AnalyzeRemoteAsync(computerVision, remoteImageUrl).GetAwaiter().GetResult();
+
+            // 本地图片
+            AnalyzeLocalAsync(computerVision, localImagePath).GetAwaiter().GetResult();
+
+
+            Console.WriteLine("按任意键键退出...");
             Console.ReadLine();
         }
 
@@ -60,24 +59,23 @@ namespace ComputerVisionImgAnalyze
         {
             if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
             {
-                Console.WriteLine(
-                    "\n无效的图片链接:\n{0} \n", imageUrl);
+                Console.WriteLine("\n无效的图片链接:\n{0} \n", imageUrl);
                 return;
             }
 
-            ImageAnalysis analysis =
-                await computerVision.AnalyzeImageAsync(imageUrl, features);
+            ImageAnalysis analysis = await computerVision.AnalyzeImageAsync(imageUrl, features);
+
             DisplayResults(analysis, imageUrl);
+            DisplayImgTag(analysis);
         }
 
-        // 分析局部图像
+        // 分析本地图像
         private static async Task AnalyzeLocalAsync(
             ComputerVisionClient computerVision, string imagePath)
         {
             if (!File.Exists(imagePath))
             {
-                Console.WriteLine(
-                    "\n无法打开或读取本地图像路径:\n{0} \n", imagePath);
+                Console.WriteLine("\n无法打开或读取本地图像路径:\n{0} \n", imagePath);
                 return;
             }
 
@@ -85,15 +83,30 @@ namespace ComputerVisionImgAnalyze
             {
                 ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
                     imageStream, features);
+
                 DisplayResults(analysis, imagePath);
+                DisplayImgTag(analysis);
             }
         }
 
         // 显示图像最相关的标题
         private static void DisplayResults(ImageAnalysis analysis, string imageUri)
         {
-            Console.WriteLine(imageUri);
-            Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
+            Console.WriteLine("\r\n\r\n{0}", imageUri);
+            foreach (var caption in analysis.Description.Captions)
+            {
+                Console.WriteLine("\r\n{0}\r\n", caption.Text);
+            }
+           
+        }
+
+        // 展示标签
+        private static void DisplayImgTag(ImageAnalysis analysis)
+        {
+            foreach (var tag in analysis.Tags)
+            {
+                Console.WriteLine("标签名称:{0}       {1}%", tag.Name, Math.Round(tag.Confidence * 100, 2));
+            }
         }
     }
 }
--- ComputerVisionOCR/Program.cs
+++ ComputerVisionOCR/Program.cs
@@ -14,17 +14,21 @@ using System.Threading.Tasks;
 
 namespace ComputerVisionOCR
 {
+    /// <summary>
+    /// OCR图片文字识别
+    /// </summary>
+
     static class Program
     {
         // Replace <Subscription Key> with your valid subscription key.
-        const string apiKey = "你的key";
+        const string apiKey = "";
         // api 地址
         const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr";
 
         static void Main()
         {
 
-            string imageFilePath = @"你的本地图片文件全路径";
+            string imageFilePath = @"imgs/1.png";
             string imgUrl = "https://leizhangstorage.blob.core.chinacloudapi.cn/azureblog/ocr.jpg";
 
 
@@ -32,24 +36,25 @@ namespace ComputerVisionOCR
             var result = string.Empty;
 
 
-            //Console.WriteLine("从文件识别:");
+            Console.WriteLine("从文件识别:");
 
-            //result = AzureOcrHelper.OcrFromFile(client, uriBase, apiKey, imageFilePath).Result;
+            result = AzureOcrHelper.OcrFromFile(client, uriBase, apiKey, imageFilePath, "zh-Hans").Result;
+            Console.WriteLine("\r\n响应:\n\n{0}\n", JToken.Parse(result).ToString());
+            result = AzureOcrHelper.SimpleFormattedText(result);
+            Console.WriteLine("\r\n格式化输出结果:\n\n{0}\n", result);
+
+            Console.WriteLine("\r\n===================================\r\n");
+
+
+            //Console.WriteLine("从url识别:");
+
+            //result = AzureOcrHelper.OcrFromUrl(client, uriBase, apiKey, imgUrl).Result;
             //Console.WriteLine("\r\n响应:\n\n{0}\n", JToken.Parse(result).ToString());
 
 
-            //Console.WriteLine("\r\n===================================\r\n");
 
-
-            Console.WriteLine("从url识别:");
-
-            result = AzureOcrHelper.OcrFromUrl(client, uriBase, apiKey, imgUrl).Result;
-            Console.WriteLine("\r\n响应:\n\n{0}\n", JToken.Parse(result).ToString());
-
-
-
-            result = AzureOcrHelper.SimpleFormattedText(result);
-            Console.WriteLine("\r\n格式化输出结果:\n\n{0}\n", result);
+            //result = AzureOcrHelper.SimpleFormattedText(result);
+            //Console.WriteLine("\r\n格式化输出结果:\n\n{0}\n", result);
 
 
 
