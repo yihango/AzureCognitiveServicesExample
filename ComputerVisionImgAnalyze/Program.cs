5e1a1d36c2319cf14cb979bbe0c076db80bda5db
stan e <yi.hang@live.com>
2018-12-03 14:18:45 +0800

    Ocr识别案例添加

---
+++ ComputerVisionImgAnalyze/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace ComputerVisionImgAnalyze
+{
+    class Program
+    {
+        // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+        private const string apiKey = "<SubscriptionKey>";
+
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"<LocalImage>";
+
+        private const string remoteImageUrl =
+            "http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg";
+
+        // Specify the features to return
+        private static readonly List<VisualFeatureTypes> features =
+            new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+            VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+            VisualFeatureTypes.Tags
+        };
+
+        static void Main(string[] args)
+        {
+            var computerVision = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(apiKey),
+                new System.Net.Http.DelegatingHandler[] { });
+
+            /*
+             You must use the same region as you used to get your subscription keys.
+             For example, if you got your subscription keys from westus,replace "westcentralus" with "westus".
+             Free trial subscription keys are generated in the "westus" region. 
+             If you use a free trial subscription key, you shouldn't need to change the region.
+             */
+
+            // 指定Azure区域
+            computerVision.Endpoint = "https://westcentralus.api.cognitive.microsoft.com";
+
+            Console.WriteLine("分析图像中 ...");
+            var t1 = AnalyzeRemoteAsync(computerVision, remoteImageUrl);
+            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
+
+            Task.WhenAll(t1, t2).Wait(5000);
+            Console.WriteLine("按ENTER键退出");
+            Console.ReadLine();
+        }
+
+        // 分析远程图像
+        private static async Task AnalyzeRemoteAsync(
+            ComputerVisionClient computerVision, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine(
+                    "\n无效的图片链接:\n{0} \n", imageUrl);
+                return;
+            }
+
+            ImageAnalysis analysis =
+                await computerVision.AnalyzeImageAsync(imageUrl, features);
+            DisplayResults(analysis, imageUrl);
+        }
+
+        // 分析局部图像
+        private static async Task AnalyzeLocalAsync(
+            ComputerVisionClient computerVision, string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine(
+                    "\n无法打开或读取本地图像路径:\n{0} \n", imagePath);
+                return;
+            }
+
+            using (Stream imageStream = File.OpenRead(imagePath))
+            {
+                ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
+                    imageStream, features);
+                DisplayResults(analysis, imagePath);
+            }
+        }
+
+        // 显示图像最相关的标题
+        private static void DisplayResults(ImageAnalysis analysis, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
+        }
+    }
+}
---
+++ ComputerVisionOCR/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ComputerVisionOCR
+{
+    static class Program
+    {
+        // Replace <Subscription Key> with your valid subscription key.
+        const string apiKey = "你的key";
+        // api 地址
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr";
+
+        static void Main()
+        {
+
+            string imageFilePath = @"你的本地图片文件全路径";
+            string imgUrl = "https://leizhangstorage.blob.core.chinacloudapi.cn/azureblog/ocr.jpg";
+
+
+            HttpClient client = new HttpClient();
+            var result = string.Empty;
+
+
+            //Console.WriteLine("从文件识别:");
+
+            //result = AzureOcrHelper.OcrFromFile(client, uriBase, apiKey, imageFilePath).Result;
+            //Console.WriteLine("\r\n响应:\n\n{0}\n", JToken.Parse(result).ToString());
+
+
+            //Console.WriteLine("\r\n===================================\r\n");
+
+
+            Console.WriteLine("从url识别:");
+
+            result = AzureOcrHelper.OcrFromUrl(client, uriBase, apiKey, imgUrl).Result;
+            Console.WriteLine("\r\n响应:\n\n{0}\n", JToken.Parse(result).ToString());
+
+
+
+            result = AzureOcrHelper.SimpleFormattedText(result);
+            Console.WriteLine("\r\n格式化输出结果:\n\n{0}\n", result);
+
+
+
+            Console.WriteLine("\n按任意键退出...");
+            Console.ReadLine();
+        }
+    }
+}
